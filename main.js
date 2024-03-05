5f2950ea61c2f068d57f516d2e8580b73804bde1
Elliotlf001 <137006835+Elliotlf001@users.noreply.github.com>
2024-03-05 13:05:37 +0000

    Update main.js

--- main.js
+++ main.js
@@ -12,6 +12,6 @@ function senddata(){
 	var username_i = form1_email.value;
 	var password_i = form1_pwd.value;
 	var contact_i = form1_contact.value;
-	alert("Your personal data has been comprimised and you have been reported to the school " + username_i + ", " + contact_i + ".");
+	alert("Completed" + username_i + ", " + contact_i + ".");
 }
 
