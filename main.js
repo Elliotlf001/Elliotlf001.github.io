e314b93140fdb6ac8a196c21836eff77203bbd0a
Elliotlf001 <137006835+Elliotlf001@users.noreply.github.com>
2024-03-05 11:10:11 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function senddata(){
+	var form1_email = document.querySelector("input[id=email]");
+	var form1_pwd = document.querySelector("input[id=pwd]");
+	var form1_contact = document.querySelector("input[id=contact]");
+	var username_i = form1_email.value;
+	var password_i = form1_pwd.value;
+	var contact_i = form1_contact.value;
+	alert("Your personal data has been comprimised and you have been reported to the school " + username_i + ", " + contact_i + ".");
+}
+
